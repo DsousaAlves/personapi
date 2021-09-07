dc09a047704d20a6afc14394b5c24e42df2beb6a
David de Sousa Alves <davidsousaalves@gmail.com>
2021-09-06 21:33:46 -0300

    Create Person CRUD

---
+++ src/main/java/dsousaalves/github/personapi/mapper/PersonMapper.java
@@ -0,0 +1,19 @@
+package dsousaalves.github.personapi.mapper;
+
+import dsousaalves.github.personapi.dto.request.PersonDTO;
+import dsousaalves.github.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
